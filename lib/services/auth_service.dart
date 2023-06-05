8c4be418821ee3daf671dba7133a51720feb82be
kwakhyun <khyun2348@naver.com>
2023-06-06 00:12:52 +0900

    Feat: 사용자 인증 API

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,47 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class AuthService {
+  final String baseUrl;
+
+  AuthService({required this.baseUrl});
+
+  Future<dynamic> join(String email, String nickname, String pw) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/buki/join'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, dynamic>{
+        'email': email,
+        'nickname': nickname,
+        'pw': pw,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to join');
+    }
+  }
+
+  Future<dynamic> login(String email, String pw) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/buki/login'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: jsonEncode(<String, dynamic>{
+        'email': email,
+        'pw': pw,
+      }),
+    );
+
+    if (response.statusCode == 200) {
+      return jsonDecode(response.body);
+    } else {
+      throw Exception('Failed to login');
+    }
+  }
+}
